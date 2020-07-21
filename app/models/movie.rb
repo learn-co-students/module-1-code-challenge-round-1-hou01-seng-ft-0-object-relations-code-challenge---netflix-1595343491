f4a715932d4d00f9d315e96e99bb0a12c5f20a60
Wilmar Reyes <reyes.wilmar@gmail.com>
2020-07-21 11:30:31 -0500

    all methods complete

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -12,4 +12,24 @@ class Movie
     @@all
   end
 
+  def reviews
+    Review.all.find_all {|i| i.movie == self}
+  end
+
+  def reviewers
+    self.reviews.map {|i| i.viewer}
+  end
+
+  def average_rating
+    array = self.reviews.map {|i| i.rating if i.movie == self}
+    rating = array.sum.to_f / array.length
+    rating.round(1)
+  end
+
+  def self.highest_rated
+    movies = Review.all.map {|i| i.movie}
+    ratings = movies.uniq.inject(Hash.new(0)) {|h,v| h[v] = v.average_rating; h}
+    ratings = ratings.sort_by {|k, v| -v}
+    ratings[0][0]
+  end
 end
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,25 @@ class Viewer
   def self.all
     @@all
   end
+
+  def reviews
+    Review.all.find_all {|i| i.viewer == self}
+  end
+
+  def reviewed_movies
+    self.reviews.map {|i| i.movie}
+  end
+
+  def reviewed_movie?(movie)
+    self.reviewed_movies.include?(movie)
+  end
+
+  def rate_movie(movie, rating)
+    if self.reviewed_movie?(movie)
+      self.reviews.find {|i| i.rating = rating if i.movie == movie}
+    else
+      Review.new(self, movie, rating)
+    end
+  end
   
 end
