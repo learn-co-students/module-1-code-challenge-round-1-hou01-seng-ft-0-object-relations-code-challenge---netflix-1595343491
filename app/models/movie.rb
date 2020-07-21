d403e92bbbc8b7a50e7b56f67bdef397752f104c
Tneebs <tedneben@gmail.com>
2020-07-21 11:51:25 -0500

    MOD1 Code Challenge Submit

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -1,11 +1,49 @@
 class Movie
-  attr_accessor :title
+  attr_accessor :title, :rating
 
   @@all = []
 
   def initialize(title)
     @title = title
-    self.class.all << self
+    @@all << self
+  end
+
+  def average_rating
+    avg_rating = Review.all.select do |reviews|
+      reviews.movie == self
+    end
+
+    ratings = avg_rating.map {|rate| rate.ratings }
+    rating_average = ratings.sum / ratings.length
+  end
+
+  # I seem to be running into an error where it is not 
+  # finding my reader/writer method for rating.
+  # I believe I am on the right path to get the average
+  # however, I believe my instances are messed up somewhere
+  # because I have a attr_accessor for rating, which tells me 
+  # it probably is throwing an error because it is pointed
+  # at the wrong thing.
+
+  def self.highest_rated
+  end
+
+  # I believe you can use .max() for this method. 
+  # You would take your Review.all and iterate through
+  # and pull out your ratings. From here you can assign that array
+  # of ratings and .max() to pull your highest value out, which will be your highest_rated.
+
+
+  def reviews
+    Review.all
+  end
+
+  def reviewers
+    Viewer.all
+  end
+
+  def title
+   @title
   end
 
   def self.all
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -8,6 +8,42 @@ class Viewer
     self.class.all << self
   end
 
+  def reviewed_movie?(movie)
+    Review.all.select do |reviews|
+     if reviews.viewer == movie
+      true
+     else
+      false
+     end
+    end
+  end
+
+  def rate_movie(movie,rating)
+    # we need to iterate over the Review.all (array) to check
+    # if Viewer instance (self in this case) and the
+    # Movie instance (argument) are associated with each other.
+    # IF they are not, then we create Review.new
+    # ELSE if Viewer instance (self) has reviewed THIS
+    # Movie (argument) then it will assign the new rating 
+    # to the existing Review instance.
+
+    # I believe we can use our previous Aggregated method (reviewed_movie?)
+    # to meet these conditions.
+
+
+  def reviewed_movies
+    Movie.all
+  end
+
+  def reviews
+    # binding.pry
+    Review.all
+  end
+
+  def username
+    @username
+  end
+
   def self.all
     @@all
   end
