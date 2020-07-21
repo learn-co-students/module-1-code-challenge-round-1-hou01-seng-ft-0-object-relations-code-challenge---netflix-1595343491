667efbbbb3596e9b07899a411b948a7517ab23a5
piaoyehong1107 <piaoyehong1107@gmail.com>
2020-07-21 11:37:06 -0500

    final

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -11,5 +11,34 @@ class Movie
   def self.all
     @@all
   end
+  
+  def reviews
+    Review.all.find_all{|value|
+    value.movie==self}
+  end
 
+  def reviewers
+    arr=Review.all.find_all{|value|
+    value.movie==self}
+    arr.map{|ele|
+    ele.viewer}
+  end
+
+  def average_rating
+   arr=Review.all.find_all{|value|
+    value.movie==self}
+    all_rating=[]
+    arr.map{|ele|
+    all_rating.push(ele.rating)}
+    average=all_rating.sum/all_rating.length.to_f
+  end
+
+  def self.highest_rated
+    all_rating=[]
+    Review.all.collect{|ele|
+    all_rating.push(ele.rating)}
+    id=all_rating.index(all_rating.max)
+    Review.all[id].movie
+  end
 end
+
--- app/models/viewer.rb
+++ app/models/viewer.rb
@@ -11,5 +11,33 @@ class Viewer
   def self.all
     @@all
   end
-  
+
+  def reviews
+    Review.all.find_all{|value|
+    value.viewer==self}
+  end
+
+  def reviewed_movies
+    arr=Review.all.find_all{|value|
+    value.viewer==self}
+    arr.each{|ele|
+    ele.movie}
+  end
+
+  def reviewed_movie?(movie)
+    titles=[]
+    self.reviewed_movies.each{|ele|
+    titles.push(ele.movie.title)}
+    titles.include? movie.title
+  end
+
+  def rate_movie(movie, rating)
+    if self.reviewed_movie?(movie)
+      rated_review=self.reviewed_movies.find{|value|
+      value.movie==movie}
+      rated_review.rating=rating
+    else
+      Review.new(self, movie,rating)
+    end
+  end
 end
