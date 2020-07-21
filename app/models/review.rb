2c0c693951367ad0492e46f742346db55ff0014a
tomtom-code <tomlai8882000@hotmail.com>
2020-07-21 12:02:21 -0500

    submit

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,4 +1,48 @@
 class Review
 
+    attr_accessor :viewer, :movie, :rating
+
+    @@all = []
+
+    def initialize(viewer, movie, rating)
+
+        @viewer = viewer
+        @movie = movie
+        @rating = rating
+
+        self.class.all << self
+        
+    end
+
+
+    def self.all
+        @@all
+    end
+
+    def viewer
+        
+        #Review.all.select{|x| x == self}
+        Review.all.each do |k,v|
+            k == self
+            
+        end
+        return @viewer
+        
+
+    end
+
+    def movie
+        Review.all.each do |k,v|
+            k == self
+            
+        end
+        return @movie
+    end
+
+
+
+
+
+
 
 end
